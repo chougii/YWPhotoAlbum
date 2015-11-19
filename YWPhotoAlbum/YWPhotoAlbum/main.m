446a86456cb8cc8d772d96051fca6b4c4304fc92
chougii <281435246@qq.com>
2015-11-19 16:47:20 +0800

    create

---
+++ YWPhotoAlbum/YWPhotoAlbum/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YWPhotoAlbum
+//
+//  Created by ChouGii on 15/11/19.
+//  Copyright © 2015年 zyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
